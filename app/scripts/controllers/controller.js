29ff9b2f080c348ae51fd593f5b47ac8386853af
Lu6ith <wojtlub@gmail.com>
2014-04-14 14:30:39 +0200

    dodano controller.js

---
+++ app/scripts/controllers/controller.js
@@ -0,0 +1,13 @@
+(function () {
+    'use strict';
+
+/* Controllers */
+
+    angular.module('myApp.controllers')
+        .controller('LineListCtrl', ['$scope', 'Line',
+        function($scope, Line) {
+            $scope.lines = Line.query();
+        //$scope.orderProp = 'age';
+        }]);
+
+}());
